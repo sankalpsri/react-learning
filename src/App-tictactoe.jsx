20507dc29255acc6efe418f6a7e2e8779053bd25
Sankalp Srivastava <sankalpsrii@gmail.com>
2026-02-13 12:55:37 +0530

    Updating gameTurns

---
+++ src/App-tictactoe.jsx
@@ -0,0 +1,111 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Players from './components/Tictactoe/Players.jsx'
+import GameBoard from './components/Tictactoe/GameBoard.jsx'
+import Logs from './components/Tictactoe/Logs.jsx'
+import { WINNING_COMBINATIONS } from './winning-combinations.js'
+import GameOver from './components/Tictactoe/GameOver.jsx'
+
+const INITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
+const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2"
+};
+
+function deriveActivePlayer(turns) {
+  let currentPlayer = "X";
+  if (turns.length > 0 && turns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+
+}
+
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map((innerArray) => [...innerArray])];
+  gameTurns.forEach((turn) => {
+    const { row, col } = turn.square;
+    gameBoard[row][col] = turn.player;
+  });
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players) {
+  let winner = null;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquare = gameBoard[combination[0].row][combination[0].column];
+    const secondSquare = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquare = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquare && firstSquare === secondSquare && firstSquare === thirdSquare) {
+      winner = players[firstSquare];
+      console.log("Winner:", firstSquare);
+      break;
+    }
+  }
+  return winner;
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  const gameBoard = deriveGameBoard(gameTurns);
+
+  const winner = deriveWinner(gameBoard, players);
+
+  const isDraw = gameTurns.length === 9 && !winner;
+
+  function handlePlayerNameChange(playerSymbol, newName) {
+    setPlayers((previousPlayers) => {
+      return {
+        ...previousPlayers,
+        [playerSymbol]: newName
+      }
+    });
+  }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns(
+      (prevousTurns) => {
+        let currentPlayer = deriveActivePlayer(prevousTurns);
+        const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevousTurns];
+        return updatedTurns;
+      });
+  }
+  return (
+    <>
+      <main className='gboard-container container-fluid text-white p-2'>
+        <div id='player' className='row hightlight-player'>
+          <Players initialName={PLAYERS.X} symbol="X" isActive={activePlayer === "X"} changePlayerName={handlePlayerNameChange} />
+          <Players initialName={PLAYERS.O} symbol="O" isActive={activePlayer === "O"} changePlayerName={handlePlayerNameChange} />
+        </div>
+        <div className='row'>
+          <div className="game-board col-12">
+            <div className="row">
+              <div className="col-md-6 offset-md-4">
+                {(winner || isDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
+                <GameBoard board={gameBoard} onSelectSquare={handleSelectSquare} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <Logs turns={gameTurns} />
+      </main >
+    </>
+  )
+}
+export default App
---
+++ src/components/Headers/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="/images/calculator-logo.png" alt="logo" className="header-logo" />
+            <h1 className="header-title">Investment Calculator</h1>
+        </header>
+    )
+}
